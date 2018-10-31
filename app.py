8b04a91cf758a20c9421928a3dd42d8206151b46
Glanis <glanis.monteiro@robosoftin.com>
2018-10-31 19:03:20 +0530

    adding app.py after importing db

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -14,9 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)
